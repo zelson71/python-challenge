c88f216c7870309de8c194140fbbe60e52907d6c
zelson71 <56380588+zelson71@users.noreply.github.com>
2020-01-05 02:19:36 -0700

    Add files via upload

--- homework/PyPoll/main.py
+++ homework/PyPoll/main.py
@@ -16,10 +16,6 @@ with open("Resources/election_data.csv") as file:
         voter.append(row[0])
         cast.append(row[2])
 
-print("Election Results")
-print("---------------------------------")
-print(f"Total Votes: {len(voter)}")
-print("---------------------------------")
 
 # get a list of distinct values of candidates in order
 cand2 = list(set(cast))
@@ -27,28 +23,23 @@ cand2.sort()
 # get a vote count for each candidate
 for cand in cand2:
     vote.append(cast.count(cand))
-
+print(f"Election Results\n"+"---------------------------------\n" +
+      "Total Votes: {len(voter)}\n"+"---------------------------------")
 for c in range(len(cand2)):
+    '''
+    {:.2%} Format percentage
+    '''
     print(
         f"{cand2[c]}: {'{:.2%}'.format(vote[c]/len(cast))} ({vote[c]})")
-print("---------------------------------")
-print(f"Winner: {cand2[vote.index(max(vote))]}")
-print("---------------------------------")
+print("---------------------------------\n" +
+      f"Winner: {cand2[vote.index(max(vote))]}\n"+"---------------------------------")
 # write to analysis file
 with open(file_to_write, "w") as txt_file:
-    txt_file.write("Voter Analysis")
-    txt_file.write("\n")
-    txt_file.write("__________________________________")
-    txt_file.write("\n")
-    txt_file.write(f"Total Votes: {len(voter)}")
-    txt_file.write("\n")
-
+    txt_file.write(f"Election Results\n"+"---------------------------------\n" +
+                   "Total Votes: {len(voter)}\n"+"---------------------------------\n")
     for c in range(len(cand2)):
+
         txt_file.write(
-            f"{cand2[c]}: {'{:.2%}'.format(vote[c]/len(cast))} ({vote[c]})")
-        txt_file.write("\n")
-    txt_file.write("__________________________________")
-    txt_file.write("\n")
-    txt_file.write(f"Winner: {cand2[vote.index(max(vote))]}")
-    txt_file.write("\n")
-    txt_file.write("__________________________________")
+            f"{cand2[c]}: {'{:.2%}'.format(vote[c]/len(cast))} ({vote[c]})\n")
+    txt_file.write("---------------------------------\n" +
+                   f"Winner: {cand2[vote.index(max(vote))]}\n"+"---------------------------------")
